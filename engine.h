68722d099e258a057b4d62f02adf789585946f19
oleggy-gvz <oleggy.gvz@gmail.com>
2019-05-07 16:53:53 +0700

    up

--- engine.h
+++ engine.h
@@ -12,10 +12,34 @@ private:
     map<double, double> V_M; // x - rotation speed (V), y - torque (M)
     double T_over; // overheating temperature
     double H_m; // coefficient of heating rate versus torque
+    double H_v; // coefficient of heating rate versus speed of crankshaft rotation
+    double C; // coefficient of cooling rates versus engine temperature and environment
 
+    double get_a(double m) // acceleration
+    {
+        return m / I;
+    }
+
+    double get_V_h(double m, double v) // heating rate
+    {
+        return m * H_m + v * v * H_v;
+    }
+
+    double get_V_c(double t_envir, double t_engine) // cooling rate
+    {
+        return C * (t_envir - t_engine);
+    }
+
+    double get_M(double V)
+    {
+        return;
+    }
 
 public:
-    virtual ~Engine() = 0;
+    void Enable()
+    {
+
+    }
 };
 
 Engine::~Engine()
